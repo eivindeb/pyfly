bd0972ed08256bebf803e78fce563d4420a2fbd5
Eivind Bøhn <eivind.bohn@gmail.com>
2019-07-02 10:36:26 +0200

    rename pyfly to avoid collision with pypi package

--- setup.py
+++ setup.py
@@ -4,13 +4,13 @@ with open('requirements.txt') as f:
     requirements = f.read().splitlines()
 
 setup(
-    name='pyfly',
+    name='pyflight',
     version='0.1.0',
     url="https://github.com/eivindeb/pyfly",
     author="Eivind Bøhn",
     author_email="eivind.bohn@gmail.com",
     description="Fixed-Wing Flight Simulator",
-    py_modules=['pyfly',],
+    py_modules=['pyflight',],
     license='MIT',
     long_description_content_type='text/markdown',
     long_description=open('README.md').read(),
