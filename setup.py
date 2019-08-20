683fdbfff421ed046a5ce095487382160ab4b489
Eivind Bøhn <eivindeb@github.com>
2019-08-20 17:03:05 +0200

    increment pyfly version number

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='pyfly',
-    version='0.1.0',
+    version='0.1.1',
     url="https://github.com/eivindeb/pyfly",
     author="Eivind Bøhn",
     author_email="eivind.bohn@gmail.com",
