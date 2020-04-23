4150d825e1e6eeec43143f73d1e6a9f86b158afb
Eivind Bøhn <eivind.bohn@gmail.com>
2020-04-23 19:53:20 +0200

    refactor dryden turbulence model, fixing bug where wrong number of steps was simulated and wrong input used

--- pyfly/dryden.py
+++ pyfly/dryden.py
@@ -3,8 +3,39 @@ from scipy.signal import lti
 import math
 
 
+class Filter:
+    def __init__(self, num, den):
+        self.filter = lti(num, den)
+        self.x = None
+        self.y = None
+        self.t = None
+
+    def simulate(self, u, t):
+        if self.x is None:
+            x_0 = None
+        else:
+            x_0 = self.x[-1]
+
+        self.t, self.y, self.x = self.filter.output(U=u, T=t, X0=x_0)
+
+        return self.y
+
+    def reset(self):
+        self.x = None
+        self.y = None
+        self.t = None
+
+
 class DrydenGustModel:
-    def __init__(self, size, dt, b, h=100, V_a=25, intensity=None):
+    def __init__(self, dt, b, h=100, V_a=25, intensity=None):
+        """
+        Python realization of the continuous Dryden Turbulence Model (MIL-F-8785C).
+        :param dt: (float) band-limited white noise input sampling time.
+        :param b: (float) wingspan of aircraft
+        :param h: (float) Altitude of aircraft
+        :param V_a: (float) Airspeed of aircraft
+        :param intensity: (str) Intensity of turbulence, one of ["light", "moderate", "severe"]
+        """
         # For fixed (nominal) altitude and airspeed
         h = h  # altitude [m]
         V_a = V_a  # airspeed [m/s]
@@ -53,11 +84,6 @@ class DrydenGustModel:
         T_w1 = math.sqrt(3.0) * L_w / V_a
         T_w2 = L_w / V_a
 
-        # Convert back to m/s in the numerator (NB: this should not carry over to angular rates below)
-        self.H_u = lti(feet2meters * K_u, [T_u, 1])
-        self.H_v = lti([feet2meters * K_v * T_v1, feet2meters * K_v], [T_v2 ** 2, 2 * T_v2, 1])
-        self.H_w = lti([feet2meters * K_w * T_w1, feet2meters * K_w], [T_w2 ** 2, 2 * T_w2, 1])
-
         K_p = sigma_w * math.sqrt(0.8 / V_a) * ((math.pi / (4 * b)) ** (1 / 6)) / ((L_w) ** (1 / 3))
         K_q = 1 / V_a
         K_r = K_q
@@ -66,9 +92,12 @@ class DrydenGustModel:
         T_q = T_p
         T_r = 3 * b / (math.pi * V_a)
 
-        self.H_p = lti(K_p, [T_p, 1])
-        self.H_q = lti([-K_w * K_q * T_w1, -K_w * K_q, 0], [T_q * T_w2 ** 2, T_w2 ** 2 + 2 * T_q * T_w2, T_q + 2 * T_w2, 1])
-        self.H_r = lti([K_v * K_r * T_v1, K_v * K_r, 0], [T_r * T_v2 ** 2, T_v2 ** 2 + 2 * T_r * T_v2, T_r + 2 * T_v2, 1])
+        self.filters = {"H_u": Filter(feet2meters * K_u, [T_u, 1]),
+                        "H_v": Filter([feet2meters * K_v * T_v1, feet2meters * K_v], [T_v2 ** 2, 2 * T_v2, 1]),
+                        "H_w": Filter([feet2meters * K_w * T_w1, feet2meters * K_w], [T_w2 ** 2, 2 * T_w2, 1]),
+                        "H_p": Filter(K_p, [T_p, 1]),
+                        "H_q": Filter([-K_w * K_q * T_w1, -K_w * K_q, 0], [T_q * T_w2 ** 2, T_w2 ** 2 + 2 * T_q * T_w2, T_q + 2 * T_w2, 1]),
+                        "H_r": Filter([K_v * K_r * T_v1, K_v * K_r, 0], [T_r * T_v2 ** 2, T_v2 ** 2 + 2 * T_r * T_v2, T_r + 2 * T_v2, 1]),}
 
         self.np_random = None
         self.seed()
@@ -78,50 +107,75 @@ class DrydenGustModel:
 
         self.noise = None
 
-        #self.noise = None
         self.vel_lin = None
         self.vel_ang = None
 
     def seed(self, seed=None):
+        """
+        Seed the random number generator.
+        :param seed: (int) seed.
+        :return:
+        """
         self.np_random = np.random.RandomState(seed)
 
-    def generate_noise(self, size):
-        noise = [math.sqrt(math.pi / self.dt) * self.np_random.standard_normal(size=size),
-                   math.sqrt(math.pi / self.dt) * self.np_random.standard_normal(size=size),
-                   math.sqrt(math.pi / self.dt) * self.np_random.standard_normal(size=size),
-                   math.sqrt(math.pi / self.dt) * self.np_random.standard_normal(size=size),
-                   ]
-
-        return noise
+    def _generate_noise(self, size):
+        return np.sqrt(np.pi / self.dt) * self.np_random.standard_normal(size=(4, size))
 
     def reset(self, noise=None):
+        """
+        Reset model.
+        :param noise: (np.array) Input to filters, should be four sequences of Gaussianly distributed numbers.
+        :return:
+        """
         self.vel_lin = None
         self.vel_ang = None
         self.sim_length = 0
+
         if noise is not None:
+            assert len(noise.shape) == 2
+            assert noise.shape[0] == 4
             noise = noise * math.sqrt(math.pi / self.dt)
         self.noise = noise
 
+        for filter in self.filters.values():
+            filter.reset()
+
     def simulate(self, length):
+        """
+        Simulate turbulence by passing band-limited Gaussian noise of length length through the shaping filters.
+        :param length: (int) the number of steps to simulate.
+        :return:
+        """
         t_span = [self.sim_length, self.sim_length + length]
 
         t = np.linspace(t_span[0] * self.dt, t_span[1] * self.dt, length)
 
         if self.noise is None:
-            noise = self.generate_noise(t.shape[0])
+            noise = self._generate_noise(t.shape[0])
         else:
-            if self.noise.shape[-1] >= length:
-                noise = self.noise[:, :length]
-            elif self.noise.shape[-1] < length:
-                noise = np.pad(self.noise, ((0, 0), (0, length - self.noise.shape[-1])), mode="wrap")
+            if self.noise.shape[-1] >= t_span[1]:
+                noise = self.noise[:, t_span[0]:t_span[1]]
+            else:
+                noise_start_i = t_span[0] % self.noise.shape[-1]
+                remaining_noise_length = self.noise.shape[-1] - noise_start_i
+                if remaining_noise_length >= length:
+                    noise = self.noise[:, noise_start_i:noise_start_i + length]
+                else:
+                    if length - remaining_noise_length > self.noise.shape[-1]:
+                        concat_noise = np.pad(self.noise,
+                                              ((0, 0), (0, length - remaining_noise_length - self.noise.shape[-1])),
+                                              mode="wrap")
+                    else:
+                        concat_noise = self.noise[:, :length - remaining_noise_length]
+                    noise = np.concatenate((self.noise[:, noise_start_i:], concat_noise), axis=-1)
 
-        vel_lin = np.array([self.H_u.output(noise[0], t)[1],
-                                   self.H_v.output(noise[1], t)[1],
-                                   self.H_w.output(noise[2], t)[1]])
+        vel_lin = np.array([self.filters["H_u"].simulate(noise[0], t),
+                            self.filters["H_v"].simulate(noise[1], t),
+                            self.filters["H_w"].simulate(noise[2], t)])
 
-        vel_ang = np.array([self.H_p.output(noise[3], t)[1],
-                                     self.H_q.output(noise[3], t)[1],
-                                     self.H_r.output(noise[3], t)[1]])
+        vel_ang = np.array([self.filters["H_p"].simulate(noise[3], t),
+                            self.filters["H_q"].simulate(noise[1], t),
+                            self.filters["H_r"].simulate(noise[2], t)])
 
         if self.vel_lin is None:
             self.vel_lin = vel_lin
