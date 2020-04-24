3bea82dbf9578cccb5f63571348eb63da4dfde11
Eivind Bøhn <eivind.bohn@gmail.com>
2020-04-24 11:55:34 +0200

    increment version number and change requirements to be higher or equal version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='pyfly',
-    version='0.1.1',
+    version='0.1.2',
     url="https://github.com/eivindeb/pyfly",
     author="Eivind Bøhn",
     author_email="eivind.bohn@gmail.com",
@@ -13,13 +13,13 @@ setup(
     long_description_content_type='text/markdown',
     long_description=open('README.md').read(),
     install_requires=[
-        "cycler==0.10.0",
-        "kiwisolver==1.1.0",
-        "matplotlib==3.1.0",
-        "numpy==1.16.4",
-        "pyparsing==2.4.0",
-        "python-dateutil==2.8.0",
-        "scipy==1.3.0",
-        "six==1.12.0"
+        "cycler>=0.10.0",
+        "kiwisolver>=1.1.0",
+        "matplotlib>=3.1.0",
+        "numpy>=1.16.4",
+        "pyparsing>=2.4.0",
+        "python-dateutil>=2.8.0",
+        "scipy>=1.3.0",
+        "six>=1.12.0"
   ],
 )
